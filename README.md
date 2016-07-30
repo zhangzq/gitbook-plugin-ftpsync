9c5d7cb7e36015488bb34c96018b634767de4d94
zhiqiang <zhang@zhiqiang.org>
2016-07-30 10:52:16 +0800

    not work on gitbook. not know why

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 sync gitbook to ftp server after each build
 
+not work yet. I do not know why.
+
 ## Install
 
 ```
--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 var ftpsync = require('ftpsync');
 
 var get_config = function (gitbook, name) {
-    config = gitbook.config.get('pluginsConfig')[name] || {};
+    return (gitbook.config && gitbook.config.get('pluginsConfig')[name]) || { "host": "zhiqiang.org",
+       "port": "21",
+       "user": "zhiqiang",
+       "remote": "/domains/zhiqiang.org/public_html/note/" };
 };
 
 module.exports = {
-    book: {
-    },
     hooks: {
         "finish": function () {
             var gitbook = this;
             var config = get_config(this, "ftpsync");
 
             ftpsync.settings = {
-                local: "./_book",
+                local: ".",
                 host: config.host,
                 port: config.port || 21,
                 remote: config.remote || '/',
@@ -29,7 +30,14 @@ module.exports = {
                 ftpsync.settings.port = 21;
             }
 
-            ftpsync.run();
+            ftpsync.run(function(err, result) {
+                console.log(err);
+                console.log(result);
+            });
+            var url = 'ftp://' + ftpsync.settings.host + ":" + ftpsync.settings.port + ftpsync.settings.remote;
+            console.log("copy '" + ftpsync.settings.local + "' to " + url);
         }
     }
 };
+
+module.exports.hooks.finish();
